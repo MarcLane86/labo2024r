3367e3b1d2e936fbf0a25525cc792bdcfda657de
marcolanese <marco.lanese@gmail.com>
2024-08-04 20:21:10 +0000

    auto commit 1       20240804 202108

--- src/workflows/719_workflow_epic_f202109_005.r
+++ src/workflows/719_workflow_epic_f202109_005.r
@@ -442,7 +442,7 @@ wf_septiembre <- function( pnombrewf )
   ts9 <- TS_strategy_base9()
   ht <- HT_tuning_epic()
 
-  fm <- FM_final_models_lightgbm_semillerio( c(ht, ts9), ranks=c(1), semillerio=20, repeticiones_exp=1 )
+  fm <- FM_final_models_lightgbm_semillerio( c(ht, ts9), ranks=c(1), semillerio=35, repeticiones_exp=1 )
   SC_scoring_semillerio( c(fm, ts9) )
   KA_evaluate_kaggle_semillerio()
 
